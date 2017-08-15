c4eed1c054514eb24355eb39c4670ecd502223c1
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-08-15 16:13:08 +0900

    Created main welcome view. Created new mentor view. Need to hook everything up.

--- MentorMe/src/main/java/com/beegeworks/education/mentorme/MainActivity.java
+++ MentorMe/src/main/java/com/beegeworks/education/mentorme/MainActivity.java
@@ -4,11 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.beegeworks.education.mentorme.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +14,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This is where the magic happens when you push the right button.
-     * Oh baby.
-     *
-     * @param view Should be the DisplayMessageActivity where our message will show
+     * This is called by the new mentor button. It sends to the activity where a
+     * user can create a new mentor profile for the first time.
      */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void setupNewMentor(@SuppressWarnings("UnusedParameters") View view) {
+        Intent intent = new Intent(this, NewMentorActivity.class);
         startActivity(intent);
     }
 }
