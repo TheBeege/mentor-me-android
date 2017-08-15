eae7ed05bd34ba721b9700074d74cebfd6a55260
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-08-15 17:05:21 +0900

    Added email and displayname to new mentor view. Made view scrollable. Made other minor fixes

--- MentorMe/src/androidTest/java/com/beegeworks/education/mentorme/ExampleInstrumentedTest.java
+++ MentorMe/src/androidTest/java/com/beegeworks/education/mentorme/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("org.learnteachcode.seoul.android.androidstudygroup", appContext.getPackageName());
+        assertEquals("com.beegeworks.education.mentorme", appContext.getPackageName());
     }
 }
